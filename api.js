8b3d0235eb586e40c6f3991edb8d14abb0ecf4f5
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-06-09 16:08:23 +0530

    GET Request

---
+++ api.js
@@ -0,0 +1,22 @@
+const client = require("./connection");
+const express = require("express");
+const app = express();
+
+app.listen(3000, function () {
+  console.log("Server is running at port", 3000);
+});
+
+client.connect();
+
+app.get("/users", function (req, res) {
+  client.query(`Select * from users`, function (err, results) {
+    if (!err) {
+      res.status(200).json({
+        success: true,
+        data: results.rows,
+      });
+    }
+  });
+
+  client.end;
+});
